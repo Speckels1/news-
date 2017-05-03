a20ce0337e6d8c32cb891fa92a02411548eb87a7
Speckels1 <specht.kelsey@gmail.com>
2017-05-03 14:31:33 -0500

    something

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	
+	$("#headline1").mouseenter(function() {
+		$("#red_bar_1").show("slide");
+	});
+
+	$("#headline1").mouseleave(function() {
+		$("#red_bar_1").hide("slide");
+	});
+
+	$("#headline2").mouseenter(function() {
+		$("#red_bar_2").show("slide");
+	});
+
+	$("#headline2").mouseleave(function() {
+		$("#red_bar_2").hide("slide");
+	});
+
+	$("#headline3").mouseenter(function() {
+		$("#red_bar_3").show("slide");
+	});
+
+	$("#headline3").mouseleave(function() {
+		$("#red_bar_3").hide("slide");
+	});
+
+	$("#headline4").mouseenter(function() {
+		$("#red_bar_4").show("slide");
+	});
+
+	$("#headline4").mouseleave(function() {
+		$("#red_bar_4").hide("slide");
+	});
+
+	$("#headline5").mouseenter(function() {
+		$("#red_bar_5").show("slide");
+	});
+
+	$("#headline5").mouseleave(function() {
+		$("#red_bar_5").hide("slide");
+	});
+
+	$("#headline6").mouseenter(function() {
+		$("#red_bar_6").show("slide");
+	});
+
+	$("#headline6").mouseleave(function() {
+		$("#red_bar_6").hide("slide");
+	});
+
+
+
+});
